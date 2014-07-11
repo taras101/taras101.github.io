fa6f7a97dd56a1acc56ea4cb7a5e4a25135e7cad
Taras101 <taras101@gmail.com>
2014-07-11 12:04:25 -0400

    mobile scrolling added

--- hamburger.js
+++ hamburger.js
@@ -23,9 +23,6 @@ $(document).ready(function() {
         //display a layer to disable clicking and scrolling on the content while menu is shown
         $('#contentLayer').css('display', 'block');
 
-        //disable all scrolling on mobile devices while menu is shown
-        $('#container').on('touchmove', function(e){e.preventDefault()});
-
         //set margin for the whole container with a JQuery UI animation
         $("#container").animate({"marginLeft": ["70%", 'easeOutExpo']}, {
             duration: 900
@@ -35,9 +32,6 @@ $(document).ready(function() {
 
     //close the menu
     $("#contentLayer").click(function() {
-
-        //enable all scrolling on mobile devices when menu is closed
-        $('#container').on('touchmove');
 
         //set margin for the whole container back to original state with a JQuery UI animation
         $("#container").animate({"marginLeft": ["0", 'easeOutExpo']}, {
